6874789c0d5f093eed375e4a12e2166f5d17b6f6
Mason Zhong <mz9865@rit.edu>
2019-10-07 12:00:27 -0400

    - Created signin.ftl - Implemented some attributes to Player class - Implemented constructor for GetLoginRoute

--- src/main/java/com/webcheckers/ui/GetLogInRoute.java
+++ src/main/java/com/webcheckers/ui/GetLogInRoute.java
@@ -1,14 +1,35 @@
 package com.webcheckers.ui;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import java.util.Objects;
+
+import com.webcheckers.util.Message;
+import spark.*;
+
 
 public class GetLogInRoute implements Route {
+
+    private static final Message SIGN_IN_CREDENTIALS = Message.info("Please use credentials to sign in.");
+
+    private final TemplateEngine templateEngine;
+
+
+    public GetLogInRoute(final TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        //
+        this.templateEngine = templateEngine;
+    }
 
     @Override
     public Object handle(Request request, Response response){
         //todo
+
+
+
+        final Session httpSession = request.session();
+
+
+
         return 6;
     }
 }
