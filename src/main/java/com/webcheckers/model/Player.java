5b781b67aac0ae130849093975db5047271db6d8
Mason Zhong <mz9865@rit.edu>
2019-10-07 20:45:27 -0400

    - Created signin.ftl - Implemented some attributes to Player class

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,4 +1,18 @@
 package com.webcheckers.model;
 
 public class Player {
+
+    //
+    // Attributes
+    //
+
+    private String name;
+
+    //
+    // Getters
+    //
+
+    public String getName() {
+        return name;
+    }
 }
