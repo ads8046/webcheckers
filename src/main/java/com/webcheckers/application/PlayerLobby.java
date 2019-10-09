e70691c62aa8c0dac5c195f9727048de3641680f
camperdue42 <43047763+camperdue42@users.noreply.github.com>
2019-10-09 09:16:45 -0400

    PlayerLobby filled

--- src/main/java/com/webcheckers/application/PlayerLobby.java
+++ src/main/java/com/webcheckers/application/PlayerLobby.java
@@ -1,4 +1,18 @@
 package com.webcheckers.application;
 
+import com.webcheckers.model.Player;
+
+import java.util.ArrayList;
+
 public class PlayerLobby {
+
+    private ArrayList<Player> players;
+
+    public PlayerLobby() {
+        players = new ArrayList<Player>();
+    }
+
+    public void addPlayer(String playerName) {
+        players.add(new Player(playerName));
+    }
 }
