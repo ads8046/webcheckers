e70c785ce32e270b01185b855eee90372941bb21
Atharva Shivankar <ads8046@g.rit.edu>
2019-10-10 15:08:13 -0400

    Added space class - completed Space class

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -2,11 +2,26 @@ package com.webcheckers.model;
 
 public class Space {
     private int cellIdx;
+    private int rowIndex;
+
     private final int max = 7;
     private final int min = 0;
 
-    public Space (int cellIdx) {
+    private boolean isEmpty;
+    private Piece currentPiece;
+
+
+
+    public Space ( int cellIdx, int rowIndex, boolean isEmpty, Piece currentPiece  ) {
         this.cellIdx = cellIdx;
+        this.isEmpty = true;
+        this.currentPiece = null;
+        this.rowIndex = rowIndex;
+
+    }
+
+    public boolean isValid(){
+        return ((this.getCellIdx() % 2) == 0) && ((this.rowIndex % 2) == 0);
     }
 
     public int getCellIdx() {
@@ -15,7 +30,7 @@ public class Space {
         }
         else {
             try {
-                throw new Exception(" ERROR: Index should be between 0 to 7");
+                throw new Exception(" ERROR: Space index must be between 0 to 7");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -23,5 +38,19 @@ public class Space {
         return 0;
     }
 
+    public void setSpaceOccupied(){
+        this.isEmpty = false;
+    }
+
+    public boolean isEmpty() {
+        return this.isEmpty;
+    }
+
+    public Piece getCurrentPiece(){
+        if (isEmpty) {
+            return null;
+        }
+        return currentPiece;
+    }
 
 }
